49eced92317725ed1579e1aeb09caab25f001042
kckc0608 <kckc0608@naver.com>
2021-12-01 16:36:40 +0900

    first commit

---
+++ test.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Windows.Forms;
+namespace Study1 {
+    class Program {
+        static void Main(string[] args) { 
+            Console.WriteLine("Hello world!!"); 
+            //MessageBox.Show("Hello!");
+	 
+        } 
+    }
+    
+    public partial class Form1 : Form {
+         public Form1() { InitializeComponent(); }
+    }
+}
