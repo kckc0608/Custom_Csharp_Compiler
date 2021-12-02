75581485063013fb9c788c55c232a37b9c1080d9
kckc0608 <kckc0608@naver.com>
2021-12-02 20:00:33 +0900

    Default Structure

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿// // Copyright (c) Microsoft. All rights reserved.
-// // Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-using System;
-using System.ComponentModel;
+﻿using System;
 using System.Windows;
 
 namespace WindowActivationAndDeactivation
---
+++ build.cmd
@@ -0,0 +1,3 @@
+@echo off
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild WPF_IDE.csproj
+Pause
