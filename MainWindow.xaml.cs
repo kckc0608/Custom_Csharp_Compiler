eebfe1738bf40207c9d62a75bb52055b4b83d954
kckc0608 <kckc0608@naver.com>
2021-12-22 13:42:33 +0900

    delete commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -88,103 +88,24 @@ namespace WindowActivationAndDeactivation
 
     public class Employee
     {
-        //////////////////////////////////////////////////////////////////////////////////////////////////// Property
-        ////////////////////////////////////////////////////////////////////////////////////////// Public
-
-        #region ID - ID
-
-        /// <summary>
-        /// ID
-        /// </summary>
         public int ID { get; set; }
-
-        #endregion
-        #region 성명 - Name
-
-        /// <summary>
-        /// 성명
-        /// </summary>
         public string Name { get; set; }
-
-        #endregion
-        #region 직위 - Designation
-
-        /// <summary>
-        /// 직위
-        /// </summary>
         public string Designation { get; set; }
-
-        #endregion
     }
 
     public class ExcelExportHelper<TEntity, TList> where TEntity : class where TList : List<TEntity>
     {
-        //////////////////////////////////////////////////////////////////////////////////////////////////// Field
-        ////////////////////////////////////////////////////////////////////////////////////////// Public
-
-        #region Field
-
-        /// <summary>
-        /// 리스트
-        /// </summary>
         public List<TEntity> List;
 
-        #endregion
-
-        ////////////////////////////////////////////////////////////////////////////////////////// Private
-
-        #region Field
-
-        /// <summary>
-        /// 응용 프로그램
-        /// </summary>
         private Microsoft.Office.Interop.Excel.Application application = null;
-
-        /// <summary>
-        /// 워크북들
-        /// </summary>
         private Microsoft.Office.Interop.Excel.Workbooks workbooks = null;
-
-        /// <summary>
-        /// 워크북
-        /// </summary>
         private Microsoft.Office.Interop.Excel._Workbook workbook = null;
-
-        /// <summary>
-        /// 시트들
-        /// </summary>
         private Microsoft.Office.Interop.Excel.Sheets sheets = null;
-
-        /// <summary>
-        /// 워크시트
-        /// </summary>
         Microsoft.Office.Interop.Excel.Worksheet worksheet = null;
-
-        /// <summary>
-        /// 범위
-        /// </summary>
         private Microsoft.Office.Interop.Excel.Range range = null;
-
-        /// <summary>
-        /// 폰트
-        /// </summary>
         private Microsoft.Office.Interop.Excel.Font font = null;
-
-        /// <summary>
-        /// 부가적인 값
-        /// </summary>
         private object optionalValue = Missing.Value;
 
-        #endregion
-
-        //////////////////////////////////////////////////////////////////////////////////////////////////// Method
-        ////////////////////////////////////////////////////////////////////////////////////////// Public
-
-        #region 생성하기 - Generate()
-
-        /// <summary>
-        /// 생성하기
-        /// </summary>
         public void Generate()
         {
             try
@@ -211,15 +132,6 @@ namespace WindowActivationAndDeactivation
             }
         }
 
-        #endregion
-
-        ////////////////////////////////////////////////////////////////////////////////////////// Private
-
-        #region 인스턴스 생성하기 - CreateInstance()
-
-        /// <summary>
-        /// 인스턴스 생성하기
-        /// </summary>
         private void CreateInstance()
         {
             this.application = new Microsoft.Office.Interop.Excel.Application();
@@ -233,16 +145,6 @@ namespace WindowActivationAndDeactivation
             this.worksheet = this.sheets[1] as Microsoft.Office.Interop.Excel.Worksheet;
         }
 
-        #endregion
-        #region 행들 추가하기 - AddRows(startRange, rowCount, columnCount, values)
-
-        /// <summary>
-        /// 행들 추가하기
-        /// </summary>
-        /// <param name="startRange">시작 범위</param>
-        /// <param name="rowCount">행 수</param>
-        /// <param name="columnCount">열 수</param>
-        /// <param name="values">값들</param>
         private void AddRows(string startRange, int rowCount, int columnCount, object values)
         {
             this.range = this.worksheet.get_Range(startRange, this.optionalValue);
@@ -252,12 +154,6 @@ namespace WindowActivationAndDeactivation
             this.range.set_Value(this.optionalValue, values);
         }       
 
-        #endregion
-        #region 헤더 스타일 설정하기 - SetHeaderStyle()
-
-        /// <summary>
-        /// 헤더 스타일 설정하기
-        /// </summary>
         private void SetHeaderStyle()
         {
             this.font = this.range.Font;
@@ -265,13 +161,6 @@ namespace WindowActivationAndDeactivation
             this.font.Bold = true;
         }
 
-        #endregion
-        #region 헤더 생성하기 - CreateHeader()
-
-        /// <summary>
-        /// 헤더 생성하기
-        /// </summary>
-        /// <returns>헤더 목록</returns>
         private object[] CreateHeader()
         {
             PropertyInfo[] propertyInfoArray = typeof(TEntity).GetProperties();
@@ -292,15 +181,6 @@ namespace WindowActivationAndDeactivation
             return headerArray;
         }
 
-        #endregion
-        #region 컬럼 너비 자동 맞추기 - AutoFitColumnWidth(startRange, rowCount, columnCount)
-
-        /// <summary>
-        /// 컬럼 너비 자동 맞추기
-        /// </summary>
-        /// <param name="startRange">시작 범위</param>
-        /// <param name="rowCount">행 수</param>
-        /// <param name="columnCount">열 수</param>
         private void AutoFitColumnWidth(string startRange, int rowCount, int columnCount)
         {
             this.range = this.worksheet.get_Range(startRange, this.optionalValue);
@@ -310,13 +190,6 @@ namespace WindowActivationAndDeactivation
             this.range.Columns.AutoFit();
         }
 
-        #endregion
-        #region 데이타 작성하기 - WriteData(headerArray)
-
-        /// <summary>
-        /// 데이타 작성하기
-        /// </summary>
-        /// <param name="headerArray">헤더 배열</param>
         private void WriteData(object[] headerArray)
         {
             object[,] dataArray = new object[List.Count, headerArray.Length];
@@ -345,12 +218,6 @@ namespace WindowActivationAndDeactivation
             AutoFitColumnWidth("A1", List.Count + 1, headerArray.Length);
         }
 
-        #endregion
-        #region 시트 채우기 - FillSheet()
-
-        /// <summary>
-        /// 시트 채우기
-        /// </summary>
         private void FillSheet()
         {
             object[] headerArray = CreateHeader();
@@ -358,24 +225,11 @@ namespace WindowActivationAndDeactivation
             WriteData(headerArray);
         }
 
-        #endregion
-        #region 엑셀 보여주기 - ShowEXCEL()
-
-        /// <summary>
-        /// 엑셀 보여주기
-        /// </summary>
         private void ShowEXCEL()
         {
             this.application.Visible = true;
         }
 
-        #endregion
-        #region 인스턴스 해제하기 - ReleaseInstance(instance)
-
-        /// <summary>
-        /// 인스턴스 해제하기
-        /// </summary>
-        /// <param name="instance">인스턴스</param>
         private void ReleaseInstance(object instance)
         {
             try
@@ -395,7 +249,5 @@ namespace WindowActivationAndDeactivation
                 GC.Collect();
             }
         }
-
-        #endregion
     }
 }
